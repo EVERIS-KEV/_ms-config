d463a7e9adfd4d97965024df3f3183838e2abc83
Mauricio06Everis <kpalmall@everis.com>
2021-08-27 22:06:46 -0500

    Update Final Azure

--- src/main/java/com/everis/configserver/MsConfigServerApplication.java
+++ src/main/java/com/everis/configserver/MsConfigServerApplication.java
@@ -1,15 +1,17 @@
 package com.everis.configserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@Slf4j
 @EnableConfigServer
 @SpringBootApplication
 public class MsConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsConfigServerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(MsConfigServerApplication.class, args);
+    log.info("SERVER CONFIG ENABLED");
+  }
 }
